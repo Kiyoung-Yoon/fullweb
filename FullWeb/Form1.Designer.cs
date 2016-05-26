bcd16bc87c231928247fbfe14baac985a23d0309
KY.Yoon <drmstorm@hanmail.net>
2016-05-27 08:48:21 +0900

    특정URL을 전체 화면으로 표시

---
+++ FullWeb/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace FullWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webControl = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webControl
+            // 
+            this.webControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webControl.Location = new System.Drawing.Point(0, 0);
+            this.webControl.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webControl.Name = "webControl";
+            this.webControl.Size = new System.Drawing.Size(674, 380);
+            this.webControl.TabIndex = 0;
+            this.webControl.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.webControl_PreviewKeyDown);
+            this.webControl.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 380);
+            this.Controls.Add(this.webControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webControl;
+    }
+}
+
---
+++ FullWeb/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FullWeb
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.Location = new Point( -500, 100 );
+
+            int posX = 0;
+            int posY = 0;
+
+            string URL = "";
+
+            try
+            {
+                FileStream fs = new FileStream("config.ini", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains("POSITION-X"))
+                    {
+                        string value = null;
+                        value = sr.ReadLine();
+                        posX = Int32.Parse(value);
+                    }
+                    if (line.Contains("POSITION-Y"))
+                    {
+                        string value = null;
+                        value = sr.ReadLine();
+                        posY = Int32.Parse(value);
+                    }
+                    if (line.Contains("URL"))
+                    {
+                        string value = null;
+                        value = sr.ReadLine();
+                        URL = value;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //만들기..
+                try
+                {
+                    FileStream fs = new FileStream("config.ini", FileMode.Create);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.WriteLine("[POSITION-X]");
+                    sw.WriteLine("0");
+                    sw.WriteLine("[POSITION-Y]");
+                    sw.WriteLine("0");
+                    sw.WriteLine("[URL]");
+                    sw.WriteLine("");
+                    sw.Close();
+                    fs.Close();
+                    MessageBox.Show("설정파일이 생성되었습니다. 설정값을 확인해주십시오.");
+                    this.Close();
+                }
+                catch (Exception ex2)
+                {
+                    MessageBox.Show("설정파일을 생성하지 못하였습니다. \r\n[config.ini] 파일을 확인해 주십시오.");
+                    this.Close();
+                }
+            }
+
+            this.Location = new Point( posX, posY );
+            
+            
+            webControl.Navigate( URL );
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {   
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {   
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void webControl_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
